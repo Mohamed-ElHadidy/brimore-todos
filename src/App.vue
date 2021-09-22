dcea153f1328ab1e855714bff462113ccab36ef8
Muhammed El-Hadidy <mohammed.elhadidy90@gmail.com>
2021-09-22 20:10:49 +0200

    feat: Added vuex and created store

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <h1>{{ title }}</h1>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+// import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    // HelloWorld,
+  },
+  computed: {
+    title() {
+      return this.$store.state.appTitle;
+    },
   },
 };
 </script>
